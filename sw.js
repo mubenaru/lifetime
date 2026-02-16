aedd7def4fd3fefe15831a7396df5c35833fb56c
mubenaru <mubenaruka1618@gmail.com>
2026-02-16 09:06:28 +0900

    sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,21 @@
-self.addEventListener('install', function(event) {
-  console.log('Service Worker installed');
+const CACHE_NAME = "life-hourglass-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
